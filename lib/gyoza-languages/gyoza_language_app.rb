80a258ceab43a071d85cef0d3d66f3b89b80221c
user <fulminazzo21@gmail.com>
2025-01-19 20:37:41 +0100

    Added template implementation of get method

--- lib/gyoza-languages/gyoza_language_app.rb
+++ lib/gyoza-languages/gyoza_language_app.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'rugged'
+require 'linguist'
+
 require_relative 'gyoza_app'
 
 # An implementation of GyozaApp that works with GitHub linguist.
@@ -29,8 +32,20 @@ class GyozaLanguageApp < GyozaApp
   #          the languages will be looked for that particular branch. However,
   #          if not found a 404 'Not Found' error is returned
   #   env: the environment variables at the time of receiving the request
-  def get(path, query, env)
-    #TODO:
+  protected def get(path, query, env)
+    repository = "#{@repo_directory}/#{path}"
+    unless File.directory?(repository)
+      #TODO: 404
+    end
+
+    repo = Rugged::Repository.new(repository)
+    if query.include? "branch"
+      #TODO: change branch and handle error
+    end
+
+    project = Linguist::Repository.new(repo, repo.head.target_id)
+    languages = project.languages
+    #TODO: return languages
   end
 
 end
