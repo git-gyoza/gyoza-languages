4c367a027a90107460fa10dc87f7e29e380ab873
user <fulminazzo21@gmail.com>
2025-01-19 23:18:53 +0100

    Added usage method and minimal logic in gyoza-languages binary

--- lib/gyoza-languages/version.rb
+++ lib/gyoza-languages/version.rb
@@ -4,4 +4,9 @@ module GyozaLanguages
   VERSION = '0.1.0'
   # The content sent as the HTTP Server header
   SERVER_NAME = "Gyoza-Languages/#{VERSION}"
+  DEFAULT_PORT = 9172
+
+  SPEC_NAME = $PROGRAM_NAME.split('/').last
+  SPEC_DESCRIPTION = 'A Ruby implementation of the Github linguist project with an integrated simple HTTP web server'
+  REPOSITORIES_DIRECTORY_ENV_NAME = 'REPOSITORIES_DIRECTORY'
 end
