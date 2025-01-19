e0cd6fa1e6111f8e275c39236bb5c6d58c163b5a
user <fulminazzo21@gmail.com>
2025-01-19 19:49:42 +0100

    Removed private constructor

--- lib/gyoza-languages/gyoza_error.rb
+++ lib/gyoza-languages/gyoza_error.rb
@@ -2,7 +2,6 @@
 
 # Represents a general error during the application execution
 class GyozaError < StandardError
-  private_class_method :new
 
   def self.invalidDirectory(directory)
     return GyozaError.new("\"#{directory}\" is not a valid directory.")
