9b44c9bcd0bb4f96e56f83f177db561f8c141a6c
user <fulminazzo21@gmail.com>
2025-01-19 20:19:40 +0100

    Added tests for call method

--- spec/gyoza-languages/gyoza_app_spec.rb
+++ spec/gyoza-languages/gyoza_app_spec.rb
@@ -5,6 +5,17 @@ require 'gyoza-languages/gyoza_error'
 
 RSpec.describe GyozaApp do
 
+  ["GET", "POST", "PUT", "PATCH", "DELETE"].each do |method|
+    it "should return 405 by default when requesting #{method}" do
+      expect(GyozaApp.new('.')
+                     .call({
+                             "REQUEST_METHOD" => method,
+                             "REQUEST_PATH" => "",
+                             "QUERY_STRING" => "" })
+      ).to eq([405, {}, []])
+    end
+  end
+
   it "is initialized with an invalid repositories directory" do
     expect { GyozaApp.new 'invalid' }.to raise_error(GyozaError)
   end
