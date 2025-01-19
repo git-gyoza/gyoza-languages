51faf2929c322e445e8db51cbe141af16647e670
user <fulminazzo21@gmail.com>
2025-01-20 00:22:42 +0100

    Set release version to 1.0.0

--- lib/gyoza-languages/version.rb
+++ lib/gyoza-languages/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module GyozaLanguages
-  VERSION = '0.1.0'
+  VERSION = '1.0.0'
   # The content sent as the HTTP Server header
   SERVER_NAME = "Gyoza-Languages/#{VERSION}"
   DEFAULT_PORT = 9172
