b266628ebf8f2c30190f18e81f715ed987423516
user <fulminazzo21@gmail.com>
2025-01-19 20:10:13 +0100

    Created StringUtils class with query_string_to_hash function

---
+++ lib/gyoza-languages/string_utils.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# A collection of utilities for strings.
+class StringUtils
+  private_class_method :new
+
+  # Converts the given query string to a Hash object.
+  # A query string is identified by a set of
+  # key-value pairs in the format "{key}={value}"
+  # separated by the '&' character.
+  #
+  # Arguments:
+  #   string: the string to convert
+  def self.query_string_to_hash(string)
+    dict = {}
+    string.split("&").map do |pair|
+      key, value = pair.split("=")
+      dict[key] = value.sub("+", " ")
+    end
+    dict
+  end
+
+end
