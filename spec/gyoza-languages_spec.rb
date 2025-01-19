3861404841aca7c58d04bc9d6d3a8ecf0c770a14
user <fulminazzo21@gmail.com>
2025-01-19 22:23:08 +0100

    Removed default tests

--- spec/gyoza-languages_spec.rb
+++ spec/gyoza-languages_spec.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 RSpec.describe GyozaLanguages do
-  it 'has a version number' do
-    expect(GyozaLanguages::VERSION).not_to be nil
-  end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
