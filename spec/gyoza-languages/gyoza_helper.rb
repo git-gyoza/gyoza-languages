22644718c73817238c1fba3b8e92978be8dc876a
user <fulminazzo21@gmail.com>
2025-01-20 20:54:20 +0100

    Created gyoza_helper module with MockHandler class

---
+++ spec/gyoza-languages/gyoza_helper.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# A collection of utilities for the tests in this directory.
+module GyozaHelper
+
+  # Represents a mock class for a Rackup Handler.
+  # Provides default run and stop methods.
+  class MockHandler
+    attr_accessor :started, :caller, :port
+
+    def initialize
+      stop
+    end
+
+    def run(caller, options = {})
+      if options.include? 'port'
+        @port = options['port']
+      end
+      @started = true
+      @caller = caller
+    end
+
+    def stop
+      @port = nil
+      @started = false
+      @caller = nil
+    end
+
+  end
+
+end
+
+# Overrides the default Rackup Handler.
+# For testing purposes only.
+module Rackup
+  module Handler
+    def self.default(options = {})
+      return GyozaHelper::MockHandler.new
+    end
+  end
+end
