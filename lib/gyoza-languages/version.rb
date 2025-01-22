c69240ab17f5b884b540f217ebf42a928b10861a
user <fulminazzo21@gmail.com>
2025-01-22 18:07:57 +0100

    Updated default port

--- lib/gyoza-languages/version.rb
+++ lib/gyoza-languages/version.rb
@@ -4,7 +4,7 @@ module GyozaLanguages
   VERSION = '1.0.4'
   # The content sent as the HTTP Server header
   SERVER_NAME = "Gyoza-Languages/#{VERSION}"
-  DEFAULT_PORT = 20125
+  DEFAULT_PORT = 2015
 
   SPEC_NAME = $PROGRAM_NAME.split('/').last
   SPEC_DESCRIPTION = 'A Ruby implementation of the Github linguist project with an integrated simple HTTP web server'
